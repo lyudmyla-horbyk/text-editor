51a467039c2b46434339b0f3c1b3a890945f0dbe
Liudmyla Liashuk <lyudmyla.horbyk@gmail.com>
2020-07-17 23:36:12 +0300

    implement text editor

--- src/components/EditorToolbar.vue
+++ src/components/EditorToolbar.vue
@@ -1,47 +1,71 @@
 <template>
   <div class="editor-tollbar">
-    <select v-model="checkedColor">
-      <option class="heading" value selected>- Color -</option>
+    <select v-model="checkedColor" @change="onColorChange">
+      <option value selected disabled>- Color -</option>
       <option value="red">Red</option>
       <option value="blue">Blue</option>
       <option value="green">Green</option>
       <option value="black">Black</option>
     </select>
-    <select v-model="checkedSize">
-      <option class="heading" value selected>- Size -</option>
-      <option value="8px">Small</option>
-      <option value="14px">Normal</option>
-      <option value="24px">Big</option>
-      <option value="36px">Very Big</option>
+    <select v-model="checkedSize" @change="onSizeChange">
+      <option value selected disabled>- Size -</option>
+      <option value="1">Small</option>
+      <option value="5">Normal</option>
+      <option value="6">Big</option>
+      <option value="7">Very Big</option>
     </select>
-    <select v-model="checkedBackground">
-      <option class="heading" value selected>- Background -</option>
+    <select v-model="checkedBackground" @change="onBackgroundChange">
+      <option value selected disabled>- Background -</option>
       <option value="red">Red</option>
       <option value="blue">Blue</option>
       <option value="green">Green</option>
       <option value="black">Black</option>
     </select>
+    <button @click="onButtonJsonClick" type="button">Copy JSON</button>
   </div>
 </template>
 <script>
 export default {
-  data: function() {
+  data() {
     return {
       checkedColor: "",
       checkedSize: "",
       checkedBackground: ""
     };
   },
-  components: {},
-  methods: {}
+  methods: {
+    onColorChange(e) {
+      this.$emit("change-color", e.target.value);
+    },
+    onSizeChange(e) {
+      this.$emit("change-size", e.target.value);
+    },
+    onBackgroundChange(e) {
+      this.$emit("change-background", e.target.value);
+    },
+    onButtonJsonClick(e) {
+      this.$emit("copy-json", e.target.value);
+    }
+  }
 };
 </script>
 <style scoped lang="scss">
 .editor-tollbar {
+  display: flex;
+  align-items: center;
   border: 1px solid grey;
   background-color: #e6e6e6;
   width: 100%;
   height: 60px;
   margin-top: 30px;
+  select {
+    padding: 3px;
+    margin-right: 10px;
+    cursor: pointer;
+  }
+  button {
+    padding: 3px;
+    cursor: pointer;
+  }
 }
 </style>
--- src/components/TextEditor.vue
+++ src/components/TextEditor.vue
@@ -1,26 +1,51 @@
 <template>
-  <p class="text-editor" ref="editable" contenteditable v-on="listeners"></p>
+  <div>
+    <EditorToolbar
+      @change-color="changeColorText"
+      @change-size="changeSizeText"
+      @change-background="changeBackgroundText"
+      @copy-json="copyJson"
+    />
+    <p class="text-editor" ref="editable" contenteditable v-html="this.value"></p>
+  </div>
 </template>
 
 <script>
+import EditorToolbar from "./EditorToolbar";
+
 export default {
+  components: {
+    EditorToolbar
+  },
   props: {
     value: {
       type: String,
       default: ""
     }
   },
-  computed: {
-    listeners() {
-      return { ...this.$listeners, input: this.onInput };
-    }
-  },
   mounted() {
     this.$refs.editable.innerText = this.value;
+    document.execCommand("styleWithCSS", false, true);
   },
   methods: {
-    onInput(e) {
-      this.$emit("input", e.target.innerText);
+    changeColorText(colorText) {
+      document.execCommand("foreColor", false, colorText);
+    },
+    changeSizeText(sizeText) {
+      document.execCommand("fontSize", false, sizeText);
+    },
+    changeBackgroundText(backgroundText) {
+      document.execCommand("backColor", false, backgroundText);
+    },
+    copyJson() {
+      console.log(this.$refs.editable.childNodes);
+      const myArray = [...this.$refs.editable.childNodes].map(x => ({
+        text: x.textContent,
+        fontSize: x.style && x.style.fontSize,
+        color: x.style && x.style.color,
+        backgroundColor: x.style && x.style.backgroundColor
+      }));
+      console.log(myArray);
     }
   }
 };
